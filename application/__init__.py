ad7023a0e27b3e0f8a5512e4ff017d644fa350f9
Clark Seanor <clarkseanor@gmail.com>
2020-03-22 20:57:42 +0000

    Upgrade to Flask

---
+++ application/__init__.py
@@ -0,0 +1,47 @@
+import os
+from flask import Flask, render_template
+from iso3166 import countries
+from .meetings import MeetingCollection, CategoryCollection, Category
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        MEETINGS_CSV=os.path.join(app.root_path, 'meetings.csv')
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Load all meetings from csv
+    meeting_collection = MeetingCollection()
+    meeting_collection.import_csv(app.config['MEETINGS_CSV'])
+
+    # the homepage
+    @app.route('/')
+    def index():
+        meetings_by_country = meeting_collection.split_by_country()
+        categories = CategoryCollection()
+
+        for country_code, meetings in meetings_by_country.items():
+            country_name = countries.get(country_code).name
+            new_category = Category(country_name, meetings.sort_by_name())
+            categories.add(new_category)
+
+        sorted_categories = categories.sort_by_name()
+
+        return render_template("index.html", categories=sorted_categories)
+
+    return app
+
---
+++ application/meetings.py
@@ -0,0 +1,89 @@
+import csv
+
+class Meeting:
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.metadata = kwargs
+
+        print(kwargs)
+
+
+
+class MeetingCollection:
+    def __init__(self, meetings=None):
+        if meetings == None:
+            self.meetings = []
+        else:
+            self.meetings = meetings
+
+    def import_csv(self, filename):
+        with open(filename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=",")
+            for row in reader:
+
+                meeting = Meeting(
+                    row['NAME'],
+                    country=row['COUNTRY'],
+                    state_province=row['STATE_PROVINCE'],
+                    city_town=row['CITY_TOWN'],
+                    link=row['LINK'],
+                    worship_format=row['WORSHIP_FORMAT'],
+                    medium=row['MEDIUM'],
+                    medium_format=row['MEDIUM_FORMAT'].strip('"'),
+                    is_live=int(row['IS_LIVE'])
+                )
+
+                self.meetings.append(meeting)
+
+    def add(self, meeting):
+        self.meetings.append(meeting)
+    
+    # ISO two-character codes
+    def filter_by_country(self, country_code):
+        meetings_by_country = [m for m in self.meetings if (meeting.metadata["country"] == country_code)]
+        return MeetingCollection(meetings_by_country)
+
+    def split_by_country(self):
+        country_groups = {}
+        
+        for meeting in self.meetings:
+            if meeting.metadata["country"] in country_groups:
+                country_groups[meeting.metadata["country"]].add(meeting)
+            else:
+                country_groups[meeting.metadata["country"]] = MeetingCollection([meeting])
+        
+        return country_groups
+
+    def to_list(self):
+        return self.meetings
+
+    def sort_by_name(self, reverse=False):
+        sorted_meetings = self.meetings.copy()
+        sorted_meetings.sort(key=lambda x: x.name, reverse=reverse)
+
+        return MeetingCollection(sorted_meetings)
+
+class Category:
+    def __init__(self, name, meetings):
+        self.name = name
+        self.meetings = meetings
+
+class CategoryCollection:
+    def __init__(self, categories=None):
+        if categories == None:
+            self.categories = []
+        else:
+            self.categories = categories
+
+    def to_list(self):
+        return self.categories
+
+    def add(self, category):
+        self.categories.append(category)
+
+    def sort_by_name(self, reverse=False):
+        sorted_categories = self.categories.copy()
+        sorted_categories.sort(key=lambda x: x.name, reverse=reverse)
+
+        return CategoryCollection(sorted_categories)
+    
